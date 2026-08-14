5193643f1424f66cffee5983a8f97a3ec6dc3c0b
D.Tuguldur <tuguldurd0627@gmail.com>
2026-08-14 21:36:11 +0800

    Add files via upload

---
+++ boom/Scripts/kill_zone.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		print("You died!")
+		Engine.time_scale = 0.2
+		$CollisionShape2D.queue_free()
+	timer.start()
+	
+	
+	
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
